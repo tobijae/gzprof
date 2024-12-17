515d98284461b0fd233548d621b55c136991c2e1
Tobiasfib <72310273+tobijae@users.noreply.github.com>
2024-12-17 14:50:49 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/gzprof',
+}
 
+module.exports = nextConfig
