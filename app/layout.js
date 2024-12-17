9967ca94996b3208a50b33883a6bfee3689c23f2
Tobiasfib <72310273+tobijae@users.noreply.github.com>
2024-12-17 14:51:29 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,17 @@
+import './globals.css'
 
+export const metadata = {
+  title: 'Professor Card Game',
+  description: 'Create professor trading cards',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Audiowide&display=swap" rel="stylesheet" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}
